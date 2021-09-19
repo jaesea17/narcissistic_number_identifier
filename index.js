747c336ee9e0edb3bf07dc80ba78df0193ef7369
jaesea <oforkajajoseph@gmail.com>
2021-09-19 20:28:12 +0100

    made one response

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const formInput = document.getElementById("form-input");
+let body = document.getElementsByTagName("body")[0];
+let paragraph = document.createElement("p");
 
 const determine = (e) => {
     e.preventDefault();
     const number = document.getElementById("input-number");
     const numberEntered = number.value;
-    let body = document.getElementsByTagName("body")[0];
-    let paragraph = document.createElement("p");
     let sum = 0;
     let singleNumber = numberEntered.split("");
     let digits = singleNumber.length;
