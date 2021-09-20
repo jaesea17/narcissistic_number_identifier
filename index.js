0b78682c8c8f00adadf6eb260aa8be7bfca04a2d
jaesea <oforkajajoseph@gmail.com>
2021-09-20 02:42:03 +0100

    validation updated

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 const formInput = document.getElementById("form-input");
+let body = document.getElementsByTagName("body")[0];
+let paragraph = document.createElement("p");
 
 const determine = (e) => {
     e.preventDefault();
     const number = document.getElementById("input-number");
     const numberEntered = number.value;
-    let body = document.getElementsByTagName("body")[0];
-    let paragraph = document.createElement("p");
     let sum = 0;
     let singleNumber = numberEntered.split("");
     let digits = singleNumber.length;
     singleNumber.map((unit)=>{
         sum += Math.pow(unit,digits);
     });
+    let isWhole = parseFloat(numberEntered);
     
-    if(sum == numberEntered){
+    if(!Number.isInteger(isWhole)){
+        paragraph.style.color = 'red'; 
+        paragraph.innerHTML = 'only positive whole numbers are accepted';
+    }
+    if(isWhole < 0 || isWhole % 1 != 0 ){
+        paragraph.style.color = 'red'; 
+        paragraph.innerHTML = 'only positive whole numbers are accepted';
+     }
+    else if(sum === isWhole){
         paragraph.style.color = 'green';
-        paragraph.innerHTML = `${numberEntered}&nbsp&nbsp&nbsp&nbsp&nbsp is a Narcissistic Number`
+        paragraph.innerHTML = `${isWhole}&nbsp&nbsp&nbsp&nbsp&nbsp is a Narcissistic Number`
     }
     else{
         paragraph.style.color = 'red';
-        paragraph.innerHTML = `${numberEntered}&nbsp&nbsp&nbsp&nbsp is not a Narcissistic Number`;
-    
+        paragraph.innerHTML = `${isWhole}&nbsp&nbsp&nbsp&nbsp is not a Narcissistic Number`;    
     }
 
     body.appendChild(paragraph);
