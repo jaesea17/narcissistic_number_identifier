e82205709dcf1b80f112e1c1b5958b044d27a3d2
jaesea <oforkajajoseph@gmail.com>
2022-02-06 16:33:17 +0100

    edit

--- index.js
+++ index.js
@@ -9,21 +9,21 @@ const determine = (e) => {
     let sum = 0;
     let singleNumber = numberEntered.split("");
     let digits = singleNumber.length;
-    singleNumber.map((unit)=>{
-        sum += Math.pow(unit,digits);
+    singleNumber.map((unit) => {
+        sum += Math.pow(unit, digits);
     });
     let isWhole = parseFloat(numberEntered);
     
-    if(!Number.isInteger(isWhole)){
+    if (!Number.isInteger(isWhole)) {
         paragraph.style.color = 'red'; 
         paragraph.innerHTML = 'only positive whole numbers above  &nbsp "0" &nbsp are accepted';
     }
-    else if(isWhole < 1 || isWhole % 1 != 0 ){
+    else if (isWhole < 1 || isWhole % 1 != 0 ) {
         paragraph.style.color = 'red'; 
         paragraph.innerHTML = 'only positive whole numbers above  &nbsp "0" &nbsp are accepted';
      }
     
-    else if(sum === isWhole){
+    else if (sum === isWhole) {
         paragraph.style.color = 'green';
         paragraph.innerHTML = `${isWhole}&nbsp&nbsp&nbsp&nbsp&nbsp is a Narcissistic Number`;
     }
